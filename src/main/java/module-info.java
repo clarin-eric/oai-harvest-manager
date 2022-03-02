13b919625cec34d6663b7d67846d261e684b5f7c
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-03-02 08:27:47 +0100

    use new module system and fix xalan missing module issue

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module oai.harvest.manager {
+    requires Saxon.HE;
+    requires SaxonUtils;
+    requires org.apache.logging.log4j;
+    requires stax2.api;
+    requires java.xml;
+    requires java.management;
+    requires org.joda.time;
+    requires java.xml.bind;
+    requires org.apache.commons.io;
+    requires xalan;
+    requires json.path;
+    requires com.google.common;
+    requires java.logging;
+    requires woodstox.core.asl;
+    requires json.smart;
+//    provides nl.mpi.oai with nl.mpi.oai.harvester.action;
+}
