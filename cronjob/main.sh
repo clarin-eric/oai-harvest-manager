8e6f83129bd6cdd4b6323e465d295eb90c61d99c
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2023-01-17 20:39:10 +0100

    update todo in main.sh

--- cronjob/main.sh
+++ cronjob/main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# TODO: 1. mdash issue in java code 2. run main.sh for the 3rd or 4th time creates wrong folder structure
+# TODO: 1. mdash issue in java code 
 source $(pwd)/.oai.env && source $(pwd)/harvest.sh oai
 #source $(pwd)/.nde.env && source $(pwd)/harvest.sh nde
 
