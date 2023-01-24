5734a52a683ada21245620d06de252016d4f83fa
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2023-01-24 12:22:35 +0100

    stop job when previous one still running

--- cronjob/main.sh
+++ cronjob/main.sh
@@ -6,7 +6,23 @@
 
 # Clariah portainer cron
 current_dir=$(cd $(dirname $0) && pwd)
+
+if [ ! -f ${current_dir} ]; then
+  touch ${current_dir}/flag
+else
+  echo "Previous harvest not yet complete, skip the current one."
+  exit 1
+fi
+
 source ${current_dir}/.oai.env && source ${current_dir}/harvest.sh oai
 source ${current_dir}/.nde.env && source ${current_dir}/harvest.sh nde
 
+if [ ! -f ${current_dir} ]; then
+  echo "Harvest completed with error: flag status not clear, please check log and files"
+else
+  echo "Harvest completed with error: clearing log"
+  rm ${current_dir}/flag
+fi
+
+echo "Job done, goodbye! Exiting job. "
 
