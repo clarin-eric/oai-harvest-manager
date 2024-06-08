666470200f6681dcfa526e8fed2ca00113c5a40c
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2024-06-08 21:07:41 +0200

    add pyfat in requirements.txt

--- cronjob/ineo_labeling/ineo_test.sh
+++ cronjob/ineo_labeling/ineo_test.sh
@@ -7,4 +7,4 @@ current_dir=$(dirname $(readlink -f $0))
 source "${current_dir}"/.ineo.env
 echo "### Ineo labeling"
 echo "### docker image [${INEO_DOCKER_IMAGE}], mapping [${INEO_MAPPING}]"
-docker run -d --rm --network traefik-public --name ineo_labeling -v ${current_dir}/ineo_labeling.py:/app/ineo_labeling.py -e INEO_MAPPING="${INEO_MAPPING}" -e SOLR_URL="${SOLR_URL}" -e SOLR_USER="${SOLR_USER}" -e SOLR_PASSWORD="${SOLR_PASSWORD}" "${INEO_DOCKER_IMAGE}"
+docker run -d --rm --network traefik-public --name ineo_labeling -v ${DATA_VOLUME_PRODDIR}:/app/proddir -v ${current_dir}/ineo_labeling.py:/app/ineo_labeling.py -e INEO_MAPPING="${INEO_MAPPING}" -e SOLR_URL="${SOLR_URL}" -e SOLR_USER="${SOLR_USER}" -e SOLR_PASSWORD="${SOLR_PASSWORD}" "${INEO_DOCKER_IMAGE}"
