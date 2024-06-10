03b439a0702560f2acbc79f8e24b1d7ad8e1d1a1
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2024-06-10 12:01:05 +0200

    mount harvested cmdi to ineo_labeling

--- cronjob/main.sh
+++ cronjob/main.sh
@@ -29,7 +29,7 @@ fi
 # ineo labeling
 source "${current_dir}"/ineo_labeling/.ineo.env
 echo "### docker image [${INEO_DOCKER_IMAGE}], mapping [${INEO_MAPPING}]"
-docker run -d --rm --network traefik-public --name ineo_labeling -v ${DATA_VOLUME_PRODDIR}:/app/proddir -e INEO_MAPPING="${INEO_MAPPING}" -e SOLR_URL="${SOLR_URL}" -e SOLR_USER="${SOLR_USER}" -e SOLR_PASSWORD="${SOLR_PASSWORD}" "${INEO_DOCKER_IMAGE}"
+docker run -d --rm --network traefik-public --name ineo_labeling -v ${DATA_VOLUME_PRODDIR:/srv/vlo-data -e INEO_MAPPING="${INEO_MAPPING}" -e SOLR_URL="${SOLR_URL}" -e SOLR_USER="${SOLR_USER}" -e SOLR_PASSWORD="${SOLR_PASSWORD}" "${INEO_DOCKER_IMAGE}"
 
 # cleanup
 if [ ! -f "${current_dir}/flag" ]; then
